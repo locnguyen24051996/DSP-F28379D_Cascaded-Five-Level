080083c8c02199feaf8607638a6f4f1825b8a787
Nguyễn Phước Lộc <108939591+locnguyen24051996@users.noreply.github.com>
2022-07-09 10:07:34 +0700

    Add files via upload

---
+++ Cascaded_PWM/Debug/LiquidCrystal_I2C.d
@@ -0,0 +1,155 @@
+# FIXED
+
+LiquidCrystal_I2C.obj: ../LiquidCrystal_I2C.c
+LiquidCrystal_I2C.obj: ../LiquidCrystal_I2C.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/inttypes.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_ti_config.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/linkage.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_inttypes.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/stdint.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/cdefs.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_types.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_types.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_stdint.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_stdint.h
+LiquidCrystal_I2C.obj: D:/NCKH/NGUYEN\ PHUOC\ LOC/Cascaded_PWM/Print.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdio.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdarg.h
+LiquidCrystal_I2C.obj: D:/NCKH/NGUYEN\ PHUOC\ LOC/Cascaded_PWM/WString.h
+LiquidCrystal_I2C.obj: D:/NCKH/NGUYEN\ PHUOC\ LOC/Cascaded_PWM/Printable.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdlib.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdlibf.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F28x_Project.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Cla_typedefs.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_device.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/assert.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdbool.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stddef.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdint.h
+LiquidCrystal_I2C.obj: C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_stdint40.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_adc.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_analogsubsys.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_cla.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_clb.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_clbxbar.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_cmpss.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_cputimer.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_dac.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_dcsm.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_dma.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_ecap.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_emif.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_epwm.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_epwm_xbar.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_eqep.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_flash.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_gpio.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_i2c.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_input_xbar.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_ipc.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_mcbsp.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_memconfig.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_nmiintrupt.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_output_xbar.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_piectrl.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_pievect.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_sci.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_sdfm.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_spi.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_sysctrl.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_upp.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_xbar.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_xint.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_can.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Examples.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_GlobalPrototypes.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_cputimervars.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Cla_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_EPwm_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Adc_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Emif_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Gpio_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_I2c_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Ipc_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Pie_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Dma_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_SysCtrl_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Upp_defines.h
+LiquidCrystal_I2C.obj: C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_defaultisr.h
+
+../LiquidCrystal_I2C.c: 
+../LiquidCrystal_I2C.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/inttypes.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_ti_config.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/linkage.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_inttypes.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_types.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/sys/_stdint.h: 
+D:/NCKH/NGUYEN\ PHUOC\ LOC/Cascaded_PWM/Print.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdio.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdarg.h: 
+D:/NCKH/NGUYEN\ PHUOC\ LOC/Cascaded_PWM/WString.h: 
+D:/NCKH/NGUYEN\ PHUOC\ LOC/Cascaded_PWM/Printable.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdlib.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdlibf.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F28x_Project.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Cla_typedefs.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_device.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/assert.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdbool.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stddef.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/stdint.h: 
+C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/include/_stdint40.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_adc.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_analogsubsys.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_cla.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_clb.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_clbxbar.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_cmpss.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_cputimer.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_dac.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_dcsm.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_dma.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_ecap.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_emif.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_epwm.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_epwm_xbar.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_eqep.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_flash.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_gpio.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_i2c.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_input_xbar.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_ipc.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_mcbsp.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_memconfig.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_nmiintrupt.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_output_xbar.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_piectrl.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_pievect.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_sci.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_sdfm.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_spi.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_sysctrl.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_upp.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_xbar.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_xint.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/headers/include/F2837xD_can.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Examples.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_GlobalPrototypes.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_cputimervars.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Cla_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_EPwm_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Adc_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Emif_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Gpio_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_I2c_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Ipc_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Pie_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Dma_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_SysCtrl_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_Upp_defines.h: 
+C:/ti/C2000Ware_4_00_00_00/device_support/f2837xd/common/include/F2837xD_defaultisr.h: 
---
+++ Cascaded_PWM/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/lib/rts2800_fpu32.lib" -l"C:/ti/ccs1010/ccs/tools/compiler/ti-cgt-c2000_20.2.1.LTS/lib/rts2800_fpu32.lib"
+
